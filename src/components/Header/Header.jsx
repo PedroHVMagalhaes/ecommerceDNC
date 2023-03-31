fe4d88bf2f0140b3ecc3a7e798ab43e36d784d10
PedroHVMagalhaes <magalhaesphvm@gmail.com>
2023-03-30 22:28:48 -0300

    Desenvolvimento do ecommerce até a lógica da página principal

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import dncLogo from "../../assets/logo-dnc.png";
+import "./index.scss"
+
+
+const Header = () => {
+  return (
+    //Block
+    <div className="header">
+        <img className='header__img' src={dncLogo} alt="dnc logo" />
+        
+    </div>
+  );
+};
+
+export default Header;
